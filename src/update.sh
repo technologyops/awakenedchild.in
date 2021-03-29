8b5d8a1d34102e4dad52a322b507fc037f870a89
techOps <inbox@techops.in>
2021-03-29 10:50:55 +0530

    website update

--- src/update.sh
+++ src/update.sh
@@ -4,6 +4,7 @@ for f in index termsofuse aboutus faq; do
   cat header $f footer > ../$f.html
 done
 
-for f in 20210318; do
-  cat header blog/$f footer > ../blog/$f.html
-done
+cd blog/
+./update.sh
+cd ..
+
