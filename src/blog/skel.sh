2ed53b42ae6e1f812bf5caf451e65cbba669d66b
techOps <inbox@techops.in>
2021-07-21 11:13:54 +0530

    website update

--- src/blog/skel.sh
+++ src/blog/skel.sh
@@ -5,14 +5,15 @@ if [ $# -lt 3 ]; then
   exit 1;
 fi
 
-echo -n "<link rel=\"canonical\" href=\"https://awakenedchild.in/blog/$1.html\">
-  <meta name=\"author\" content=\"awakenedchild: $2\">
-  <meta name=\"description\" content=\"$3\">
-  <meta property=\"og:url\" content=\"https://awakenedchild.in/blog/$1.html\">
-  <meta property=\"og:description\" content=\"$3\">
-  <meta property=\"og:title\" content=\"$3\">" > "$1.head"
-
 echo -n "<title>awakened child: $3</title>
+<link rel=\"canonical\" href=\"https://awakenedchild.in/blog/$1.html\">
+<meta name=\"author\" content=\"awakenedchild: $2\">
+<meta name=\"description\" content=\"$3\">
+<meta property=\"og:url\" content=\"https://awakenedchild.in/blog/$1.html\">
+<meta property=\"og:description\" content=\"$3\">
+<meta property=\"og:title\" content=\"$3\">" > "$1.head"
+
+echo -n "
     <div class=\"card\">
       <div class=\"card-header\">
         <div class=\"card-title h5\">$3</div>
