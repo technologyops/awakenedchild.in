f2480581d3c9e3c52696c8781cf6240762853742
techOps <inbox@techops.in>
2022-09-06 08:51:41 +0530

    website update

--- src/gallery/update.sh
+++ src/gallery/update.sh
@@ -92,6 +92,15 @@ cat <<EOF>>$OUTFILE
   width: 100%;
   z-index: 1;
 }
+.slides figcaption {
+  position: absolute;
+  bottom: 0;
+  margin: 0 auto;
+  flex-self: end;
+  font-size: 1.5rem;
+  background-color: rgba(255,255,255,0.5);
+  padding: .5rem;
+}
 </style>
 </head>
 <body>
@@ -124,26 +133,42 @@ cat <<EOF>>$OUTFILE
   <div class="slides">
 EOF
 
-flist=("apeejay_model_town-22_october_2021-being_respectful-word_web-p2" \
-  "apeejay_noida_sc_16_a-2_feb_2022-2_Feb_2022-doing_our_best-p4" \
-  "apeejay_panchsheel-27_jul_2022-p18" \
-  "apeejay_panchsheel-27_jul_2022-p4" \
+tlist=("Class Activity-Apeejay Panchsheel, Delhi-July 2022" \
+"Baseline Survey-Abhyuday Global School, Nagpur, Maharashtra-November 2021" \
+"Class Observation-Cherrapunjee-Meghalaya-March 2022" \
+"Class Activity-Arth Foundation-Gurugram, Haryana-" \
+"Enabling Session-Jaspal Kaur School, Delhi" \
+"Home activity-Apeejay Noida-16 September 2021" \
+"Home Activity-APS Sankar Vihar-May 2022" \
+"Training-Ramakrishna Mission-Cherrapunjee-Meghalaya-December 2021" \
+"Role play-Apeejay Panchsheel-August 2022" \
+"Class Observation-Sliverline Prestige School-7 July 2022" \
+"Word Web-Apeejay Model Town-Jalandhar, Punjab-22 October 2021" \
+"Class Activity-Apeejay Noida,UP-February 2022" \
+"Home Activity-Apeejay Noida, UP-September 2021" \
+"Class Activity-Apeejay Panchsheel-July 2022")
+
+flist=("apeejay_panchsheel-27_jul_2022-p18" \
   "baseline_survey-bbhyuday_global_school_nagpur_maharashtra" \
   "cherrapunjee-meghalaya" \
   "class_observation-arth_foundation" \
   "enabling_session-jaspal_kaur_school-23-04-2022" \
   "home_activity-apeejay_noida_sec16a-up-16_sept_2021-class1-p3" \
   "home_activity-aps_sankar_vihar-11_may_2022-reyaansh_n_manonkar-cl-5a-p1" \
-  "ppeejay_noida_sec16A-up-16_sept_2021-class1" \
   "ramakrishna_mission-cherrapunji-meghalaya-16_dec_2021-p1" \
   "role_play-apeejay_panchsheel-10_aug_2022-p3" \
-  "sliverline_prestige_school_070722")
+  "sliverline_prestige_school_070722" \
+  "apeejay_model_town-22_october_2021-being_respectful-word_web-p2" \
+  "apeejay_noida_sc_16_a-2_feb_2022-2_Feb_2022-doing_our_best-p4" \
+  "ppeejay_noida_sec16A-up-16_sept_2021-class1" \
+  "apeejay_panchsheel-27_jul_2022-p4")
 
 for i in "${!flist[@]}"; do
   cat <<EOF >> $OUTFILE
-    <div class="slides-item slide-$((i+1))" id="slide-$((i+1))" aria-label="slide $((i+1)) of ${#flist[@]}" tabindex="0">\
+    <figure class="slides-item slide-$((i+1))" id="slide-$((i+1))" aria-label="slide $((i+1)) of ${#flist[@]}" tabindex="0">\
     <img src="/images/2022/09/04/${flist[$i]}.jpg" class="img-responsive" width="1200" height="900"></img>
-    </div>
+    <figcaption>${tlist[$i]}</figcaption>    
+    </figure>
 EOF
 done
 
